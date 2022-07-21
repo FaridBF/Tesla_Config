53587cc05047dd02175ce7840cebd975c5d5e38c
Farid <bouras.farid@outlook.fr>
2022-07-21 10:02:34 +0200

    build:"first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const cars = [
+    { name: 'Grande autonomie', price: 90700 },
+    { name: 'Performance', price: 106700 }
+  ];
+  const colors = [
+    { name: 'Blanc nacré Multicouches', price: 0 },
+    { name: 'Noir uni ', price: 1000 }
+  ];
+
+  // const [valueCar, setValueCar] = useState(cars[0].price);
+  // const [valueColor, setValueColor] = useState(colors[0].price);
+  const [counter, setCounter] = useState(valueCar + valueColor);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <h1>Tesla Config</h1>
+        <h2>Sélectionnez votre véhicule</h2>
+        {cars.map((car, index) => {
+          return (
+            <button
+              key={index}
+              className='button'
+              onClick={() => {
+                // setValueCar(car.price);
+                setCounter(valueColor + valueCar);
+              }}
+            >
+              {car.name} - {car.price} €
+            </button>
+          );
+        })}
+        <h2>Sélectionnez la couleur</h2>
+        {colors.map((color, index) => {
+          return (
+            <button
+              key={index}
+              className='button'
+              onClick={() => {
+                // setValueColor(color.price);
+                setCounter(valueCar + valueColor);
+              }}
+            >
+              {color.name} - {color.price}
+            </button>
+          );
+        })}
+        <div className='buy'>
+          <p>{counter}</p>
+          <button>Buy</button>
+        </div>
+      </div>
+    </>
   );
 }
 
